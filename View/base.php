6e19c9dc56a5892971d04bc5ce482669cf81ea40
DamienOlivier567 <73164252+DamienOlivier567@users.noreply.github.com>
2022-03-16 15:50:16 +0100

    Initial commit

---
+++ View/base.php
@@ -0,0 +1,58 @@
+
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="/assets/css/style.css">
+    <title>Ados en Avesnois</title>
+</head>
+<body>
+<header>
+    <div>
+        <img src="/img/logo.jpg" alt="logo de La 3CA">
+        <span>Communauté de Commune du Coeur de l'Avesnois</span>
+    </div>
+    <nav>
+        <ul>
+            <li><a href="">ACCUEIL</a></li>
+            <li><a href="">ACTIVITES SPORTIVES</a></li>
+            <li><a href="">SORTIES CULTURELLES</a></li>
+            <li><a href="">LE NUMERIQUE</a></li>
+            <li><a href="">UTILE</a></li>
+            <li><a href="">
+                    <img id="glass" src="/img/blue-glass.png" alt="">
+                </a></li>
+            <li><a href="">
+                    <img id="logoUser" src="/img/blueUser.png" alt="">
+                </a></li>
+        </ul>
+    </nav>
+</header>
+<section>
+    <div id="sport">
+        <article>
+            <a href=""></a>
+            <span>ACTIVITES SPORTIVES</span>
+        </article>
+    </div>
+    <div id="culture">
+        <article>
+            <span>SORTIES CULTURELLES</span>
+        </article>
+    </div>
+    <div id="numerical">
+        <article>
+            <span>LE NUMERIQUE</span>
+        </article>
+    </div>
+    <div id="useful">
+        <article>
+            <span>UTILE</span>
+        </article>
+    </div>
+</section>
+</body>
+</html>
