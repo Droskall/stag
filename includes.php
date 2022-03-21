6797589e294769fd99f6ee0cf435492a7a29cd84
Lyz03 <lizoe@netcourrier.com>
2022-03-21 15:55:33 +0100

    includes

--- includes.php
+++ includes.php
@@ -1,5 +1,14 @@
 <?php
 
+require __DIR__ . "/Model/Entity/Activity.php";
+require __DIR__ . "/Model/Entity/User.php";
+
+require __DIR__ . "/Model/DB.php";
+
+require __DIR__ . "/Model/Manager/Traits/ManagerTrait.php";
+require __DIR__ . "/Model/Manager/ActivityManager.php";
+require __DIR__ . "/Model/Manager/UserManager.php";
+
 require __DIR__ . "/Controller/AbstractController.php";
 require __DIR__ . "/Controller/ErrorController.php";
 require __DIR__ . "/Controller/HomeController.php";
