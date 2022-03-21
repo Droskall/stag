baa3afd2ae6fa1c5ca2fa65096197a3d159fd610
Lyz03 <lizoe@netcourrier.com>
2022-03-21 16:44:31 +0100

    preview prototype

--- View/category/category.php
+++ View/category/category.php
@@ -1,5 +1,10 @@
-<div class="activity-preview">
-    <span class="circle">15</span>
-    <h2>Intitulé de l'activité</h2>
-    <div class="small-image" style="background-image: "></div>
+<div class="preview_container">
+    <div class="activity_preview">
+        <div class="flex">
+            <h2>Intitulé de l'activité</h2>
+            <span class="circle">15</span>
+        </div>
+
+        <div class="small-image" style="background-image: "></div>
+    </div>
 </div>
