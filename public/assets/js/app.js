1addeb7d576f2139a7f5fa08ca378b113ba2fd04
Sylvie-B <unsolo@hotmail.fr>
2022-03-21 16:11:46 +0100

    bubble effect

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -14,3 +14,14 @@ menuSpan.addEventListener('click', () => {
     }
 
 })
+
+// bubbles effect
+let bubbles = document.getElementById('band').getElementsByTagName('a');
+
+for (let i = 0 ; i < bubbles.length ; i++){
+    bubbles[i].addEventListener('click', function (e){
+        e.preventDefault();
+        this.classList.toggle("grow");
+    })
+}
+
