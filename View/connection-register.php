1dd51d25250066e77786cc96f013420f35928243
Lyz03 <lizoe@netcourrier.com>
2022-03-22 10:37:40 +0100

    connection / register

---
+++ View/connection-register.php
@@ -0,0 +1,22 @@
+<h1>Connexion / Inscription</h1>
+
+<div class="flex connection_register">
+    <form id="connection" action="" method="post">
+        <h2>Connexion</h2>
+        <input type="email" placeholder="Votre email" name="mail">
+        <input type="password" placeholder="Votre mot de passe" name="password">
+
+        <input type="submit" name="submit" value="connexion">
+    </form>
+
+    <form id="register" action="" method="post">
+        <h2>Inscription</h2>
+        <input type="email" placeholder="Votre email" name="email">
+        <input type="text" placeholder="Votre pseudo" name="name">
+
+        <input type="password" placeholder="Votre mot de passe" name="password">
+        <input type="password" placeholder="Répéter votre mot de passe" name="passwordRepeat">
+
+        <input type="submit" name="submit" value="inscription">
+    </form>
+</div>
