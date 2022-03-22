7104d9c4f2bd1834a048431ea489f6feb2f6e6f5
Sylvie-B <unsolo@hotmail.fr>
2022-03-22 14:50:00 +0100

    suppr double

--- View/toolbox.php
+++ View/toolbox.php
@@ -1,26 +1,29 @@
 <section>
-    <a href="">
-        <div>
-            <h2>Clubs et associations</h2>
-            <img src="" alt="logo club">
-        </div>
-    </a>
-    <a href="">
-        <div>
-            <h2>Evénements récurrents</h2>
-            <img src="" alt="calendar logo">
-        </div>
-    </a>
-    <a href="">
-        <div>
-            <h2>Lieux à connaître</h2>
-            <img src="" alt="town logo">
-        </div>
-    </a>
-    <a href="">
-        <div>
-            <h2>Utiles</h2>
-            <img src="" alt="useful logo">
-        </div>
-    </a>
+    <div id="toolbox" class="flex">
+        <a class="white" href="">
+            <div>
+                <h2>Clubs et associations</h2>
+                <img src="" alt="logo club">
+            </div>
+        </a>
+        <a class="white" href="">
+            <div>
+                <h2>Evénements récurrents</h2>
+                <img src="" alt="calendar logo">
+            </div>
+        </a>
+        <a class="white" href="">
+            <div>
+                <h2>Lieux à connaître</h2>
+                <img src="" alt="town logo">
+            </div>
+        </a>
+        <a class="white" href="">
+            <div>
+                <h2>Utiles</h2>
+                <img src="" alt="useful logo">
+            </div>
+        </a>
+    </div>
+
 </section>
