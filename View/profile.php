088bf5d1f67cc67c4cc243a0d85b707d21b7af9a
DamienOlivier567 <73164252+DamienOlivier567@users.noreply.github.com>
2022-03-24 09:18:37 +0100

    Admin menu

--- View/profile.php
+++ View/profile.php
@@ -38,55 +38,58 @@
 
                 </div>
             </div>
-            <div id="admin" class="flex">
-                <h2>Administrateur</h2>
-                <div>
-                    <div id="add-activity">
-                        <h3>Ajouter un article</h3>
-                        <form action="" method="post" enctype="multipart/form-data">
+
+            <?php if($_SESSION["user"]->getRole() === "admin"){?>
+                <div id="admin" class="flex">
+                    <h2>Administrateur</h2>
+                    <div>
+                        <div id="add-activity">
+                            <h3>Ajouter un article</h3>
+                            <form action="" method="post" enctype="multipart/form-data">
+                                <div>
+                                    <label for="title">Titre :</label>
+                                    <input type="text" id="title" name="title">
+                                </div>
+                                <div>
+                                    <label for="activity-type">Type :</label>
+                                    <select name="activity-type" id="activity-type">
+                                        <option value="sport">Activité sportive</option>
+                                        <option value="cultural">Activité culturelle</option>
+                                        <option value="numerical">Numérique</option>
+                                    </select>
+                                </div>
+                                <div>
+                                    <label for="picture"></label>
+                                    <input type="file" id="picture" name="picture" accept=".image/jpeg, .jpg, .png">&nbsp;(Max : 2Mo)
+                                </div>
+                                <textarea name="content" id="content" cols="30" rows="15"></textarea>
+                                <div>
+                                    <input type="submit" name="addAct">
+                                </div>
+                            </form>
+                        </div>
+                        <div id="add-link">
+                            <h3>Ajouter un lien dans la boîte à outils</h3>
                             <div>
-                                <label for="title">Titre :</label>
-                                <input type="text" id="title" name="title">
-                            </div>
-                            <div>
-                                <label for="activity-type">Type :</label>
-                                <select name="activity-type" id="activity-type">
-                                    <option value="sport">Activité sportive</option>
-                                    <option value="cultural">Activité culturelle</option>
-                                    <option value="numerical">Numérique</option>
+                                <label for="link-type">Type</label>
+                                <select name="link-type" id="link-type">
+                                    <option value="club">Club et Association</option>
+                                    <option value="event">Evênement</option>
+                                    <option value="place">Lieu</option>
+                                    <option value="useful">Utile</option>
                                 </select>
                             </div>
                             <div>
-                                <label for="picture"></label>
-                                <input type="file" id="picture" name="picture" accept=".image/jpeg, .jpg, .png">&nbsp;(Max : 2Mo)
+                                <label for="newUrl"></label>
+                                <input type="url" id="newUrl" name="new-url" placeholder="lien">
                             </div>
-                            <textarea name="content" id="content" cols="30" rows="15"></textarea>
                             <div>
-                                <input type="submit" name="addAct">
+                                <input type="submit" name="addLink">
                             </div>
-                        </form>
-                    </div>
-                    <div id="add-link">
-                        <h3>Ajouter un lien dans la boîte à outils</h3>
-                        <div>
-                            <label for="link-type">Type</label>
-                            <select name="link-type" id="link-type">
-                                <option value="club">Club et Association</option>
-                                <option value="event">Evênement</option>
-                                <option value="place">Lieu</option>
-                                <option value="useful">Utile</option>
-                            </select>
-                        </div>
-                        <div>
-                            <label for="newUrl"></label>
-                            <input type="url" id="newUrl" name="new-url" placeholder="lien">
-                        </div>
-                        <div>
-                            <input type="submit" name="addLink">
                         </div>
                     </div>
                 </div>
-            </div>
+            <?php }?>
         </div>
     </div>
 </section>
