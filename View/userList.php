316c9a046c1e8cb759d846336844734f317c2eb7
Sylvie-B <unsolo@hotmail.fr>
2022-03-24 12:10:48 +0100

    setting user list

--- View/userList.php
+++ View/userList.php
@@ -1,18 +1,27 @@
 <section id="userList" class="flex white">
     <div>
         <h2>Liste des utilisateurs</h2>
-        <div class="content">
-            <ul>
-                <?php
-                foreach ($data as $item){?>
-                    <li>
-                        <?= $item->getUsername() ?>
-                    </li>
-                <?php
-                }
-                ?>
-            </ul>
+        <div class="list flex">
+            <?php
+            foreach ($data as $item){?>
+                <div class="frame flex">
+                    <img src="/assets/img/avatar.png" width="50px" alt="user logo">
+                    <div>
+                        <p><?= $item->getUsername() ?></p>
+                        <p><?= $item->getEmail() ?></p>
+                        <div class="flex">
+                            <p><?= $item->getRole() ?></p>
 
+                            <select name="userRole" id="userRole">
+                                <option value="">admin</option>
+                                <option value="user">user</option>
+                            </select>
+                        </div>
+                    </div>
+                </div>
+            <?php
+            }
+            ?>
         </div>
     </div>
 </section>
