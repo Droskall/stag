a0b73b73afacd2c653abea9aadc246c89b7f374a
Lyz03 <lizoe@netcourrier.com>
2022-03-24 16:44:37 +0100

    preview button style

--- View/category.php
+++ View/category.php
@@ -1,10 +1,10 @@
 <?php
 $category = array_pop($data);
 ?>
-<div>
-    <button style="background-color: <?= $color ?>"><a href="/index.php?c=category&a=get-category&name=<?= $category ?>&type=club">Clubs</a></button>
-    <button style="background-color: <?= $color ?>"><a href="/index.php?c=category&a=get-category&name=<?= $category ?>&type=event">Événements</a></button>
-    <button style="background-color: <?= $color ?>"><a href="/index.php?c=category&a=get-category&name=<?= $category ?>&type=place">Lieux</a></button>
+<div class="preview white" style="border-color: <?= $color ?>">
+    <button class="preview" style="background-color: <?= $color ?>"><a href="/index.php?c=category&a=get-category&name=<?= $category ?>&type=club">Clubs</a></button>
+    <button class="preview" style="background-color: <?= $color ?>"><a href="/index.php?c=category&a=get-category&name=<?= $category ?>&type=event">Événements</a></button>
+    <button class="preview" style="background-color: <?= $color ?>"><a href="/index.php?c=category&a=get-category&name=<?= $category ?>&type=place">Lieux</a></button>
 </div>
 
 <div class="flex content">
@@ -19,7 +19,7 @@ foreach ($data as $value) {
     ?>
     <div class="preview_container">
         <a href="/index.php?c=activity&id=<?= $value['activity']->getId() ?>">
-            <div class="activity_preview white">
+            <div class="activity_preview white" style="border-color: <?= $color ?>">
                 <div class="flex">
                     <h2><?= $value['activity']->getName() ?></h2>
                     <span class="circle"><?= $value['interactions'] ?></span>
