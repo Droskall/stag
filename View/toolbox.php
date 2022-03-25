296651054968faaa798da3ae7d3739ad574d063c
Sylvie-B <unsolo@hotmail.fr>
2022-03-25 15:15:47 +0100

    css tool

--- View/toolbox.php
+++ View/toolbox.php
@@ -3,16 +3,19 @@
         <a class="flex" href="">
             <div>
                 <h2>Santé</h2>
+                <img src="/assets/img/toolLogo/sante.jpg" alt="logo health">
             </div>
         </a>
         <a class="flex" href="">
             <div>
                 <h2>Mobilité</h2>
+                <img src="/assets/img/toolLogo/mobilite01.jpeg" alt="logo health">
             </div>
         </a>
         <a class="flex" href="">
             <div>
                 <h2>Aide</h2>
+                <img src="/assets/img/toolLogo/help01.jpg" alt="logo help">
             </div>
         </a>
     </div>
