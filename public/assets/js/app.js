e3031132e612115733d1425b6d6746f46d22e2d7
DamienOlivier567 <73164252+DamienOlivier567@users.noreply.github.com>
2022-03-30 13:34:54 +0200

    anim bules

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -33,14 +33,6 @@ if (close) {
 }
 
  //bubbles effect
- let bubbles = document.getElementById('band').getElementsByTagName('a');
-
- for (let i = 0 ; i < bubbles.length ; i++){
-     bubbles[i].addEventListener('click', function (e){
-        e.preventDefault();
-        this.classList.toggle("loader");
-     })
- }
 
 
 // FormConnexion/Inscription verif
