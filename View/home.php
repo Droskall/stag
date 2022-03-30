05bc324f0a474ddb76e267447408ca97e7c4e979
DamienOlivier567 <73164252+DamienOlivier567@users.noreply.github.com>
2022-03-30 11:52:45 +0200

    anim bules

--- View/home.php
+++ View/home.php
@@ -1,16 +1,20 @@
 <section id="band">
-    <a href="/index.php?c=category&a=get-category&name=sport&type">
-        <div></div>
-    </a>
-    <a href="/index.php?c=category&a=get-category&name=cultural&type">
-        <div></div>
-    </a>
-    <a href="/index.php?c=category&a=get-category&name=numerical&type">
-        <div></div>
-    </a>
-    <a href="/index.php?c=category">
-        <div></div>
-    </a>
+
+        <a class="loader" href="/index.php?c=category&a=get-category&name=sport&type">
+            <div></div>
+        </a>
+
+        <a class="loader" href="/index.php?c=category&a=get-category&name=cultural&type">
+            <div></div>
+        </a>
+
+        <a class="loader" href="/index.php?c=category&a=get-category&name=numerical&type">
+            <div></div>
+        </a>
+
+        <a class="loader" href="/index.php?c=toolbox">
+            <div></div>
+        </a>
 </section>
 <section class="white flex" id="homeTitle">
     <h1 class="flex" id="title"><span>So</span><span>D'Avesnois !</span></h1>
