514efcf4f55f6b195c0220f726c838d399d4be28
Lyz03 <lizoe@netcourrier.com>
2022-03-31 14:17:11 +0200

    user list

--- View/userList.php
+++ View/userList.php
@@ -1,35 +1,34 @@
-<section id="userList" class="flex">
-    <div class="white">
-        <h2>Liste des utilisateurs</h2>
-        <div class="list flex">
-            <?php
-            foreach ($data as $item){?>
-                <div class="frame flex">
-                    <!--   TODO replace by avatar   -->
-                    <img src="/assets/img/avatar.png" width="50px" alt="user logo">
+<section id="userList">
+    <h2>Liste des utilisateurs</h2>
+    <div class="flex">
+        <?php
+        foreach ($data as $item) {
+            ?>
+            <div class="frame flex white">
+                <img src="/assets/img/avatar/<?= $item->getAvatar() ?>" width="50px" alt="user logo">
+                <div>
+                    <p>Nom : <?= $item->getUsername() ?></p>
+                    <p>Email : <?= $item->getEmail() ?></p>
+                    <p>Role : <?= $item->getRole() ?></p>
                     <div>
-                        <p class="userList">Nom : <?= $item->getUsername() ?></p>
-                        <p class="userList">Email : <?= $item->getEmail() ?></p>
-                        <div class="flex">
-                            Role : <p><?= $item->getRole() ?></p>
-                            <form action="/index.php?c=user&a=update" method="post">
-                                <input type="hidden" name="id" value="<?= $item->getId() ?>">
-                                <select name="userRole" id="userRole">
-                                    <option value="admin">admin</option>
-                                    <option value="user">user</option>
-                                </select>
-                                <input type="submit" name="update" value="Valider">
-                            </form>
-                            <form action="/index.php?c=user&a=delete" method="post">
-                                <input type="hidden" name="id" value="<?= $item->getId() ?>">
-                                <input type="submit" name="suppr" value="Supprimer">
-                            </form>
-                        </div>
+                        <form action="/index.php?c=user&a=update" method="post">
+                            <input type="hidden" name="id" value="<?= $item->getId() ?>">
+                            <select name="userRole" id="userRole">
+                                <option value="admin">admin</option>
+                                <option value="user">user</option>
+                            </select>
+                            <input type="submit" name="update" value="Valider">
+                        </form>
+                        <form action="/index.php?c=user&a=delete" method="post">
+                            <input type="hidden" name="id" value="<?= $item->getId() ?>">
+                            <input type="submit" name="suppr" value="Supprimer l'utilisateur">
+                        </form>
                     </div>
                 </div>
+            </div>
             <?php
-            }
-            ?>
-        </div>
+        }
+        ?>
     </div>
+
 </section>
